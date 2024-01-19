601247cc69368c43b6b5a645b5ab24e6d953c971
Thomas Schollenberger <iamzenthial@gmail.com>
2024-01-19 00:32:08 -0500

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 // reference: https://en.wikipedia.org/wiki/Executable_and_Linkable_Format
 #![allow(dead_code)]
-use core::panic;
+mod bytes;
+
 use std::fs;
+use crate::bytes::*;
+
 
 #[derive(Debug)]
 enum Bits {
@@ -45,28 +48,6 @@ struct Elf {
     sec_header_size: u16,
     sec_entries: u16,
     sec_names_idx: u16,
-}
-
-fn as_u64_le(array: &[u8]) -> u64 {
-    ((array[0] as u64) << 0)
-        + ((array[1] as u64) << 8)
-        + ((array[2] as u64) << 16)
-        + ((array[3] as u64) << 24)
-        + ((array[4] as u64) << 32)
-        + ((array[5] as u64) << 40)
-        + ((array[6] as u64) << 48)
-        + ((array[7] as u64) << 56)
-}
-
-fn as_u32_le(array: &[u8]) -> u32 {
-    ((array[0] as u32) << 0)
-        + ((array[1] as u32) << 8)
-        + ((array[2] as u32) << 16)
-        + ((array[3] as u32) << 24)
-}
-
-fn as_u16_le(array: &[u8]) -> u16 {
-    ((array[0] as u16) << 0) + ((array[1] as u16) << 8)
 }
 
 fn read_elf(bytes: Vec<u8>) -> Elf {
