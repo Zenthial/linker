7224df0fd5f9058975358a0ecfa81980e662fa07
Thomas Schollenberger <iamzenthial@gmail.com>
2024-01-20 01:23:54 -0500

    remove references out of main.rs and into readme

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,3 @@
-// reference: https://en.wikipedia.org/wiki/Executable_and_Linkable_Format
-// elf32 abi specification: https://www.sco.com/developers/devspecs/gabi41.pdf
-// elf64 abi specification: https://uclibc.org/docs/elf-64-gen.pdf
-
 #![allow(dead_code)]
 mod byte_reader;
 mod elf;
